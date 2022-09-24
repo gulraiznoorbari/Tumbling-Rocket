526380fe7a857abddda0f1636544d876057dfa57
Gulraiz <gulraiznoorbari@gmail.com>
2022-09-24 20:29:56 +0500

    Add Files

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+	private GameObject _startPipe;
+	private float _timer = 0;
+	private int _score;
+
+	[SerializeField] private GameObject _pipe;
+	[SerializeField] private TextMeshProUGUI _scoreText;
+	[SerializeField] private float _startOffset;
+	[SerializeField] private float _maxTime = 1f;
+	[SerializeField] private float _height;
+
+	private void Awake()
+	{
+		Application.targetFrameRate = 60;
+		_score = 0;
+		_scoreText.SetText(_score.ToString());
+	}
+
+	private void Start()
+	{
+		transform.position = new Vector3(transform.position.x , Random.Range(-_height, _height), 0);
+		_startPipe = Instantiate(_pipe, transform.position, Quaternion.identity);
+	}
+
+	private void Update()
+	{
+		if (_timer > _maxTime)
+		{
+			transform.position = new Vector3(transform.position.x + _startOffset, Random.Range(-_height, _height), 0);
+			_startPipe = Instantiate(_pipe, transform.position, Quaternion.identity);
+
+			_timer = 0;
+		}
+		_timer += Time.deltaTime;
+	}
+
+	public void IncreaseScore()
+	{
+		_score++;
+		_scoreText.SetText(_score.ToString());
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+	private Touch _touch;
+	private Rigidbody2D _rigidbody;
+	private Vector2 _startPos;
+	private float gravity;
+	private bool dead;
+
+	public static PlayerMovement instance;
+
+	private void Start()
+	{
+		instance = this;
+		_rigidbody = GetComponent<Rigidbody2D>();
+		_startPos = transform.position;
+		gravity = _rigidbody.gravityScale;
+	}
+
+	private void Update()
+	{
+		if (dead) return;
+
+		// rotation towards velocity direction:
+		Vector2 velocity = _rigidbody.velocity;
+		float angle = Mathf.Atan2(velocity.y, 10) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));
+
+		if(Input.GetKey(KeyCode.Space) || Input.GetMouseButton(0))
+		{
+			_rigidbody.AddForce(Vector2.up * gravity * Time.deltaTime * 1700f);
+		}
+
+		if (Input.touchCount > 0)
+		{
+			_touch = Input.GetTouch(0);
+			if (_touch.phase == TouchPhase.Began)
+			{
+				_rigidbody.AddForce(Vector2.up * gravity * Time.deltaTime * 1700f);
+			}
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.gameObject.CompareTag("ScoreDetector"))
+		{
+			FindObjectOfType<GameManager>().IncreaseScore();
+		}
+	}
+
+	private void OnCollisionEnter2D(Collision2D other)
+	{
+		if (other.gameObject.layer == LayerMask.NameToLayer("Obstruction"))
+		{
+			dead = true;
+			//Handheld.Vibrate();
+			_rigidbody.AddForce(Vector2.down * 200f);
+			_rigidbody.AddTorque(30f);
+			if (other.gameObject.CompareTag("Lower Obstacle") || other.gameObject.CompareTag("Ground"))
+			{
+				_rigidbody.freezeRotation = true;
+				StartCoroutine(freezeTime());
+			}
+			//StartCoroutine(Restart());
+		}
+	}
+	private IEnumerator freezeTime()
+	{
+		yield return new WaitForSeconds(1f);
+		_rigidbody.velocity = Vector2.zero;
+		_rigidbody.freezeRotation = false;
+		Time.timeScale = 0;
+	}
+
+	public void Restart()
+	{
+		transform.position = _startPos;
+		dead = false;
+	}
+}
